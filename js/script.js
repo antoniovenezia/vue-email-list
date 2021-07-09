a0ac8fc25363e497274965262b3ef8600ad9eccc
antoniovenezia <antoniovenezia00@gmail.com>
2021-07-09 09:13:36 +0200

    vue-email-list

---
+++ js/script.js
@@ -0,0 +1,29 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            emails: [],
+
+            // emailRandom: "", 
+        },
+
+        created() {
+            this.getEmail();
+        },
+
+        methods: {
+            getEmail: function () {
+                for (let i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {  // appena il server ci risponde, si attiva questa funzione
+
+                            this.emails.push(response.data.response);
+                        });
+                };
+            },
+        },
+    });
+
+console.log();
